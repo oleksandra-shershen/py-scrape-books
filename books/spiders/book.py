b2830ba3281358c114d32fc5c36f52e3b230ada0
Shershen Alexandra <hornet240204@gmail.com>
2024-07-08 00:06:02 +0300

    Create separate methods and constants

--- books/spiders/book.py
+++ books/spiders/book.py
@@ -1,6 +1,14 @@
 import scrapy
 from scrapy.http import Response
 
+RATING_MAP = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5
+}
+
 
 class BookSpider(scrapy.Spider):
     name = "book"
@@ -17,25 +25,43 @@ class BookSpider(scrapy.Spider):
         if next_page_url is not None:
             yield response.follow(next_page_url, callback=self.parse)
 
-    @staticmethod
-    def parse_book_detail(book: Response):
-        rate = {
-            "One": 1,
-            "Two": 2,
-            "Three": 3,
-            "Four": 4,
-            "Five": 5
-        }
+    def parse_book_detail(self, response: Response):
         yield {
-            "title": book.css("h1::text").get(),
-            "price": float(book.css(".price_color::text").get().replace("£", "")),
-            "amount_in_stock": int(
-                book.css(".availability::text").re_first(r'\d+')
-            ),
-            "rating": rate[book.css(
-                ".star-rating::attr(class)"
-            ).get().split()[-1]],
-            "category": book.css(".breadcrumb > li > a::text").getall()[-1],
-            "description": book.css(".product_page > p::text").get(),
-            "upc": book.css(".table > tr").css("td::text").get()
+            "title": self.get_title(response),
+            "price": self.get_price(response),
+            "amount_in_stock": self.get_amount_in_stock(response),
+            "rating": self.get_rating(response),
+            "category": self.get_category(response),
+            "description": self.get_description(response),
+            "upc": self.get_upc(response)
         }
+
+    @staticmethod
+    def get_title(response: Response) -> str:
+        return response.css("h1::text").get()
+
+    @staticmethod
+    def get_price(response: Response) -> float:
+        price_str = response.css(".price_color::text").get().replace("£", "")
+        return float(price_str)
+
+    @staticmethod
+    def get_amount_in_stock(response: Response) -> int:
+        return int(response.css(".availability::text").re_first(r'\d+'))
+
+    @staticmethod
+    def get_rating(response: Response) -> int:
+        rating_class = response.css(".star-rating::attr(class)").get().split()[-1]
+        return RATING_MAP.get(rating_class, 0)
+
+    @staticmethod
+    def get_category(response: Response) -> str:
+        return response.css(".breadcrumb > li > a::text").getall()[-1]
+
+    @staticmethod
+    def get_description(response: Response) -> str:
+        return response.css(".product_page > p::text").get()
+
+    @staticmethod
+    def get_upc(response: Response) -> str:
+        return response.css(".table > tr").css("td::text").get()
